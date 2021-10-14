3d408abcbd80b3648b1ea44f96c78e4999a9aa8c
krishpranav <krisna.pranav@gmail.com>
2021-10-14 19:15:26 +0530

    tests: test rest api

--- tests/testrestapi.py
+++ tests/testrestapi.py
@@ -1,1 +1,12 @@
-print('hello world')
+import os
+import time
+
+def testing():
+    print('checking go version :)')
+    os.system('go version')
+    time.sleep(1)
+    os.system('go test')
+
+
+if __name__ == "__main__":
+    testing()
