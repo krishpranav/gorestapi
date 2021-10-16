754b8886762f828278be740455a38c87965bc846
krishpranav <krisna.pranav@gmail.com>
2021-10-16 17:06:14 +0530

    views: src App vue

--- views/src/App.vue
+++ views/src/App.vue
@@ -22,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgba(8, 8, 8, 0.99);
   margin-top: 60px;
 }
 </style>
