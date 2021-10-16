727c068547e1d9acc32e1c9324fd8fd466371482
krishpranav <krisna.pranav@gmail.com>
2021-10-16 14:11:28 +0530

    views

--- views/src/App.js
+++ views/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/krishpranav/gorestapi"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FRONTEND OF REST API
         </a>
       </header>
     </div>
