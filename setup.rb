626ecbd420ad7fa6680e400f64ae9b683152a2a2
krishpranav <krisna.pranav@gmail.com>
2021-10-16 16:41:36 +0530

    setup ruby script

--- setup.rb
+++ setup.rb
@@ -5,19 +5,42 @@ class Install
         sleep 1
         system('go mod tidy')
         sleep 1
+        puts "Go modules installed successfully and going to start the server"
+        sleep 1
         system('go run main.go')
     end
 
 end
 
 class InstallFrontend
-    # need to develop this block
+    def installfrontendfunc
+        sleep 1
+        system('cd views')
+        sleep 1
+        system('yarn install && yarn build')
+        sleep 1
+        puts "frontend website has been builded successfully :)"
+    end
 end
+
+def callbackinstallclass
+    installcall = Install.new
+    installcall.installrestapi
+end
+
+def callbackfrontendclass
+    installfrontend = InstallFrontend.new
+    installfrontend.installfrontendfunc
+end
+
+
 
 
 def main
-    function = Install.new
-    function.installrestapi
+    callbackfrontendclass
+    sleep 1
+    callbackinstallclass
 end
+
 
 main
