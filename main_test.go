63a38edca1c39527c8c2bf6dd6a3eb794f33662c
krishpranav <krisna.pranav@gmail.com>
2021-10-16 16:45:43 +0530

    main: tests

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"	
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 	"testing"
 )
 
+/* perform a test request */
 func performRequest(t *testing.T) {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -12,5 +14,24 @@ func performRequest(t *testing.T) {
 			"message": "pong",
 		})
 	})
-	r.Run() 
+	r.Run()
+
+
+}
+
+/* perform a test file for checking frontend is working */
+func performFrontend(t *testing.T) {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./views/dist", true)))
+
+	testapi := r.Group("/test")
+	{
+		testapi.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "test",
+			})
+		})
+	}
+
+	r.Run()
 }
