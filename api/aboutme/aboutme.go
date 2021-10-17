9cd0e064efb45e57490c3e0b72b4c1f628599cb9
krishpranav <krisna.pranav@gmail.com>
2021-10-17 11:44:30 +0530

    api: aboutme -> profile

--- api/aboutme/aboutme.go
+++
@@ -1,8 +0,0 @@
-package aboutme
-
-type Aboutme struct {
-	User string `json:"user"`
-	JoinedAt float64 `json:"joined-at"`
-	Toptracks string `json:"toptracks"`
-	Following string `json:"following"`
-}
