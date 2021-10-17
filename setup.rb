1db7863518f0f04288a3a1d9b22c22a7916a1a1e
krishpranav <krisna.pranav@gmail.com>
2021-10-17 09:32:56 +0530

    setup script

--- setup.rb
+++ setup.rb
@@ -12,33 +12,13 @@ class Install
 
 end
 
-class InstallFrontend
-    def installfrontendfunc
-        sleep 1
-        system('cd views')
-        sleep 1
-        system('yarn install && yarn build')
-        sleep 1
-        puts "frontend website has been builded successfully :)"
-    end
-end
-
 def callbackinstallclass
     installcall = Install.new
     installcall.installrestapi
 end
 
-def callbackfrontendclass
-    installfrontend = InstallFrontend.new
-    installfrontend.installfrontendfunc
-end
-
-
-
 
 def main
-    callbackfrontendclass
-    sleep 1
     callbackinstallclass
 end
 
