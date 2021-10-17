444d36feeaa19f0a8e962ff1240aeafab70f7bb2
krishpranav <krisna.pranav@gmail.com>
2021-10-17 13:57:05 +0530

    views: Vue -> ReactJS

---
+++ views/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          need to develop the frontend.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          check out the github link for gorestapi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
