6915418bae8fce880315ea49d3f49013a21b5f92
krishpranav <krisna.pranav@gmail.com>
2021-10-17 14:08:45 +0530

    src: app js

--- views/src/App.js
+++ views/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          need to develop the frontend.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          check out the github link for gorestapi
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           need to develop the frontend.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           check out the github link for gorestapi
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class myComponent extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'http://localhost:8080/albums';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+
+  render() {
+    return <h1> my Component has mounted, check the browser 'console' </h1>;
+  }
 }
 
-export default App;
+export default myComponent;
+
+// export default App;
