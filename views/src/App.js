fe3a4c0890359ac50b5919f14df2fe3d03f4e91b
krishpranav <krisna.pranav@gmail.com>
2021-10-17 14:24:48 +0530

    views: src App
    added function for calling the api
    http://localhost:8080/albums

--- views/src/App.js
+++ views/src/App.js
@@ -1,41 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import  ReactDOM  from 'react-dom';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           need to develop the frontend.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           check out the github link for gorestapi
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+class AlbumComponenet extends React.Component{
+  constructor(props){
+    super(props);
 
-class myComponent extends React.Component {
+    this.state={
+      albums:[]
+    };
+  }
+
   componentDidMount() {
-    const apiUrl = 'http://localhost:8080/albums';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
+    fetch("http://localhost:8080/albums")
   }
 
   render() {
-    return <h1> my Component has mounted, check the browser 'console' </h1>;
+    return(
+      <div>
+        <h2>Albums Details</h2>
+      </div>
+    )
   }
 }
 
-export default myComponent;
-
-// export default App;
+export default AlbumComponenet;
