e0ca889db6160935827f8ca8eeb7be79e338699a
krishpranav <krisna.pranav@gmail.com>
2021-10-17 12:23:00 +0530

    views: src

--- views/src/App.vue
+++ views/src/App.vue
@@ -22,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: rgba(8, 8, 8, 0.99);
+  color: #020202FF;
   margin-top: 60px;
 }
 </style>
--- views/src/components/HelloWorld.vue
+++ views/src/components/HelloWorld.vue
@@ -31,6 +31,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #020202FF;
 }
 </style>
