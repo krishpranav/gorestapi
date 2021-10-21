95dacce27100530f54f48187b7847a011923b420
krishpranav <krisna.pranav@gmail.com>
2021-10-21 17:31:50 +0530

    api: home recommended test class
    test class for recommend api

--- api/home/recommended_test.go
+++ api/home/recommended_test.go
@@ -1,1 +1,21 @@
 package home
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+var RecommendTracks = []Recommended {
+	{TopTracks: "TrackOne", ArtistComposed: "Artist One", Plays: 1000, DateReleased: 2020},
+}
+
+func recommendedtest(t *testing.T, err error) {
+
+	fmt.Println(RecommendTracks)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
