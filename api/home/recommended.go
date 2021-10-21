ece7c81ec0f586afb74ada76445c461fa44e4d13
krishpranav <krisna.pranav@gmail.com>
2021-10-21 17:24:58 +0530

    api: home recommended song

--- api/home/recommended.go
+++ api/home/recommended.go
@@ -1,5 +1,8 @@
 package home
 
 type Recommended struct {
-
+	TopTracks string `json:"TopTracks"`
+	Plays string `json:"plays"`
+	ArtistComposed string `json:"artistcomposed"`
+	DateReleased string `json:"datereleased"`
 }
