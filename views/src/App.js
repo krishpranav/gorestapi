23851e8ef950b0863761fac9af70d5c90d2e7381
krishpranav <krisna.pranav@gmail.com>
2021-10-27 18:05:44 +0530

    views: src App js
    // TODO need to update the ui of frontend

--- views/src/App.js
+++ views/src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          View The Api Requests In The Console
-        </p>
-
-      </header>
-    </div>
-  );
+class myComponent extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'http://localhost:8080/about';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>Check The Api Request In The Console Soon The Frontend Will Be Updated With New UI</h1>;
+  }
 }
-
-export default App;
+export default myComponent;
