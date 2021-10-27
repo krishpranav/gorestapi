35101ce156ed52d7bc668f6fd791e4401a34433d
krishpranav <krisna.pranav@gmail.com>
2021-10-27 17:48:01 +0530

    src: App js + css

--- views/src/App.js
+++ views/src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          View The Api Requests In The Console
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
