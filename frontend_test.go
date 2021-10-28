19d978fee8e2bdaec1c929064fe51b4e828d294f
krishpranav <krisna.pranav@gmail.com>
2021-10-28 17:36:20 +0530

    frontend test class

---
+++ frontend_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+/* frontend test class */
+func frontendtest(t *testing.T, err error) {
+	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./views/build", true)))
+
+	testapi := r.Group("/test")
+	{
+		testapi.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "test",
+			})
+		})
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	r.Run()
+}
