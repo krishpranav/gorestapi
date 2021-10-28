aa288ae2f867ede086cb29cf924fa511bf016595
krishpranav <krisna.pranav@gmail.com>
2021-10-28 17:04:02 +0530

    api: aboutapi test class
    imporved test classes

--- api/aboutapi/aboutapi_test.go
+++ api/aboutapi/aboutapi_test.go
@@ -5,10 +5,14 @@ import (
 	"testing"
 )
 
-var AboutVar = []Aboutapi {
-	{App: "Test"},
+var AboutVar = []Aboutapi{
+	{App: "Test", Github: "https://github.com/krishpranav/gorestapi", Author: "Krishpranav", Version: 4},
 }
 
-func AboutTest(t *testing.T) {
+func AboutTest(t *testing.T, err error) {
 	fmt.Println(AboutVar)
+
+	if err != nil {
+		panic(err)
+	}
 }
