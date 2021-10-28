69a9a3415592f859865cb8f477f4a754dba2b4e1
krishpranav <krisna.pranav@gmail.com>
2021-10-28 17:36:08 +0530

    main test class

--- main_test.go
+++ main_test.go
@@ -5,30 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
-
-/* perform a test file for checking frontend is working */
-func performFrontend(t *testing.T, err error) {
-	r := gin.Default()
-	r.Use(static.Serve("/", static.LocalFile("./views/dist", true)))
-
-	testapi := r.Group("/test")
-	{
-		testapi.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "test",
-			})
-		})
-	}
-
-	if err != nil {
-		panic(err)
-	}
-
-	r.Run()
-}
 
 /* perform post request */
 func postRequest(t *testing.T, err error) {
