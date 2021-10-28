0b53d42b44ba158bdcf9e4bf6b8df80a9bbc358f
krishpranav <krisna.pranav@gmail.com>
2021-10-28 16:39:30 +0530

    views: src App js
    changes: mycomponenet -> RestApiFrontend class name changed
    + album api request function added and called it :)

--- views/src/App.js
+++ views/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class myComponent extends React.Component {
+class RestApiFrontend extends React.Component {
+
   componentDidMount() {
     const apiUrl = 'http://localhost:8080/about';
     fetch(apiUrl)
@@ -10,19 +11,28 @@ class myComponent extends React.Component {
       .then((data) => console.log('This is your data', data));
   }
 
+  albumapirequest() {
+    const albumrequest = 'http://localhost:8080/albums'
+    fetch(albumrequest)
+      .then((response) => response.json())
+      .then((data) => console.log('Album Request'));
+  }
+
   render() {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          View The Console For Api Request
-        </p>
-      </header>
-    </div>
-  );
+    this.albumapirequest()
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            View The Console For Api Request
+          </p>
+        </header>
+      </div>
+    );
 
   }
 }
-export default myComponent;
+export default RestApiFrontend;
